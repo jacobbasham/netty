ff640f4ccf7e4be57b68ef5aa318503208e61667
Tim Boudreau <timothy.boudreau@oracle.com>
2017-06-24 18:05:48 -0400

    Add test

--- codec-dns/src/test/java/io/netty/handler/codec/dns/names/NameLengthsTest.java
+++ codec-dns/src/test/java/io/netty/handler/codec/dns/names/NameLengthsTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.handler.codec.dns.names;
 
 import io.netty.buffer.ByteBuf;
@@ -29,7 +44,8 @@ public class NameLengthsTest {
                 + codec, lookFor, read.toString());
     }
 
-    private void testRawPunycode(String name, String expected) throws UnmappableCharacterException, DnsDecoderException {
+    private void testRawPunycode(String name, String expected)
+            throws UnmappableCharacterException, DnsDecoderException {
         NameCodec writeCodec = NameCodec.get(READ_TRAILING_DOT, WRITE_TRAILING_DOT, PUNYCODE);
         NameCodec readCodec = NameCodec.get(READ_TRAILING_DOT, WRITE_TRAILING_DOT);
         ByteBuf buf = Unpooled.buffer(name.length() + name.length() / 2);
@@ -37,7 +53,6 @@ public class NameLengthsTest {
         CharSequence read = readCodec.readName(buf);
         assertEquals("Non match '" + name + "' returned '" + read + "' by "
                 + writeCodec, expected, read.toString());
-
     }
 
     @Test
